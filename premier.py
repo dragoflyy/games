57b6fee7ccc966d20541e05cf6a83aa4dfe9f46b
dragoflyy <leo.quentel@free.fr>
2014-07-22 22:46:32 +0200

    Update premier.py

--- premier.py
+++ premier.py
@@ -1,35 +1,70 @@
 # tests ici
-import time, Tkinter, os, random, pygame
+import time, Tkinter, os, random
 from Tkinter import *
+from Tkinter.messagebox import *
 # a completer x)
 # Bon fait ce que tu veut  moi j'fais un jeu de carte avec python :3 je te montrerais si tu veut P.S: suprimme cette ligne.
 # Propose qq chose je sait pas quoi faire :/
+# Et fait autre chose :) moi je viens de faire qq chose montre moi c que tu peut faire
 
 
 
-
-  # theoriquement ca fait un rond d'une couleur aléatoire la ou tu clique :3
-def change_c() :
+  # ca fait un rond d'une couleur aléatoire la ou tu clique :3
+def change_c(canvas) :
   global color     # choix d'une couleurs aléatoire :3
-  couleurs=["blue","red","green","grey","black"]
+  couleurs=["blue","red","green","grey","yellow", "light yellow"]
   i = random.randint(0,4)
   color = couleurs[i]
+  canvas.itemconfig(cercle, fill=color)
+  
   
 def get_pos(event) :
-  can.coords(oval,event.x-r,event.y-r,event.x+r,event.y+r) 
+  can.coords(cercle,event.x-r,event.y-r,event.x+r,event.y+r)
+  change_c(can)
 
   
 def create(x,y,r,color) :
   can1.create_oval(x-r,y-r,x+r,y+r,outline=color) # creation du cercle avec les couleurs et la position pre-definit...
   
 
-x,y,r=10,10,10
-fen = Tkinter.Tk()
-fen.title("Color clic")
-can = Canvas(fen, width=100, height=100, bg="black" )
-can.bind("<Button-1>",getpos)
-change_c()
-can.pack()
-fen.mainloop() 
+def rond_color() :
+  global x,y,r,fen,can,cercle
+  x,y,r=10,10,10
+  fen = Tkinter.Tk()
+  fen.title("Color clic")
+  can = Canvas(fen, width=300, height=300, bg="black" )
+  color="white"
+  cercle = can.create_oval(x, y, x+30, y+30, width=2, fill=color)
+  change_c(can)
+  can.bind("<Button-1>",get_pos)
+  can.pack()
+  fen.mainloop()
+  
+  
+def start_rond() :
+  global home
+  home.destroy
+  present = Tkinter.Tk()
+  present.title("Color clic")
+  text = Label(text="cliquez sur le carré pour faire apparaitre un rond de couleur aléatoire...")
+  contin = Button(text="continuer", command=present.destroy )
+  text.pack()
+  contin.pack()
+  present.mainloop()
+  rond_color()
+# voila...
+def quit_home() :
+  d = 1
+d = 0
+while d == 0 :
+# menu des jeux :3
+global home
+home = Tk()
+home.title("Home")
+quit = Button(text="Quitter", command=quit_home(), width=50, height=20)
+rond = Button(text="Rond aléatoire", command=start_rond(), width=50, height=20)
+
+
+
 
 
