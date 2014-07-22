9d0f5b71b20046987ba717ad88b94c4a04b1dcc2
dragoflyy <leo.quentel@free.fr>
2014-07-22 19:18:20 +0200

    Update premier.py

--- premier.py
+++ premier.py
@@ -2,4 +2,6 @@
 import time, Tkinter, os
 from Tkinter import *
 # a completer x)
+# Bon fait ce que tu veut  moi j'fais un jeu de carte avec python :3 je te montrerais si tu veut P.S: suprimme cette ligne.
+
 
