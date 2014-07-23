7043b1889fc6c89fe2e98d07a7435d53498a2821
dragoflyy <leo.quentel@free.fr>
2014-07-23 17:49:08 +0200

    Update lunch.py

--- lunch.py
+++ lunch.py
@@ -1,19 +1,20 @@
-# tests ici
+
 import time, Tkinter, os, random
 from Tkinter import *
-# a completer x)
-# Bon fait ce que tu veut  moi j'fais un jeu de carte avec python :3 je te montrerais si tu veut P.S: suprimme cette ligne.
-# Propose qq chose je sait pas quoi faire :/
-# Et fait autre chose :) moi je viens de faire qq chose montre moi c que tu peut faire
+# here is a 
 
 print("Bite")
 #J'ai perdu toutes mes notions en python :o
 
-  # ca fait un rond d'une couleur aléatoire la ou tu clique :3
+
+
+
+  # first prog.
+  # click on the square to make a rond with random color
 def change_c(canvas) :
-  global color     # choix d'une couleurs aléatoire :3
-  couleurs=["blue","red","green","grey","yellow", "light yellow","light red","light green","light blue"]
-  i = random.randint(0,8)
+  global color     # made a random color :3
+  couleurs=["blue","red","green","grey","yellow", "light yellow"]
+  i = random.randint(0,4)
   color = couleurs[i]
   canvas.itemconfig(cercle, fill=color)
   
@@ -24,44 +25,63 @@ def get_pos(event) :
 
   
 def create(x,y,r,color) :
-  can1.create_oval(x-r,y-r,x+r,y+r,outline=color) # creation du cercle avec les couleurs et la position pre-definit...
-  
+  can.create_oval(x-r,y-r,x+r,y+r,fill=color)
+
+def create_new():
+  x = random.randint(1,300)
+  y = random.randint(1,300)
+  r = 20
+  change_c(can)
+  create(x,y,r,color)
 
 def rond_color() :
   global x,y,r,fen,can,cercle
   x,y,r=10,10,10
   fen = Tkinter.Tk()
   fen.title("Color clic")
+  quit = Button(text="quit", command=fen.destroy, height=3, width=5 )
+  aleat = Button(text="add", command=create_new, height=3, width=5 )
   can = Canvas(fen, width=300, height=300, bg="black" )
   color="white"
   cercle = can.create_oval(x, y, x+30, y+30, width=2, fill=color)
   change_c(can)
   can.bind("<Button-1>",get_pos)
   can.pack()
+  quit.pack(side=LEFT)
+  aleat.pack(side=RIGHT)
   fen.mainloop()
+  menu()
   
   
 def start_rond() :
   home.destroy()
   present = Tkinter.Tk()
   present.title("Color clic")
-  text = Label(present, text="cliquez sur le carré pour faire apparaitre un rond de couleur aléatoire...")
-  contin = Button(present, text="continuer", command=present.destroy, height=2, width=4 )
+  text = Label(present, text="cliquez sur le carré pour faire apparaitre un rond de couleur aléatoire... /n click on the black squarel to made a rond with a random color...")
+  contin = Button(present, text="continue", command=present.destroy, height=2, width=4 )
   text.pack()
   contin.pack()
   present.mainloop()
   rond_color()
-# voila...
+# ...
 
-# menu des jeux :3
-global home
-home = Tkinter.Tk()
-home.title("Home")
-quit = Button(text="Quitter", command=home.destroy, width=10, height=5)
-rond = Button(text="Rond aléatoire", command=start_rond, width=10, height=5)
-quit.pack(side=RIGHT)
-rond.pack(side=LEFT)
-home.mainloop()
+def menu() : # home of games :3
+  global home
+  home = Tkinter.Tk()
+  home.title("Home")
+  quit = Button(text="Quit", command=home.destroy, width=10, height=5)
+  rond = Button(text="random rond", command=start_rond, width=10, height=5)
+  quit.pack(side=RIGHT)
+  rond.pack(side=LEFT)
+  home.mainloop()
+
+menu()
+
+
+
+
+
+
 
 
 
